c614f24cae9b4372d5c317661818367dd9f3ee03
INewBieZ <99117553+INewBieZ@users.noreply.github.com>
2022-02-23 17:54:19 +0700

    Create Math Functions.py

---
+++ Math Functions.py
@@ -0,0 +1,24 @@
+import math
+
+pi = 3.14
+
+x = 2.14
+y = 2.71
+z = 2.42
+
+print(round(pi))
+#to round the number to the closest one
+print(math.ceil(pi))
+#to round the number to the upper
+print(math.floor(pi))
+#to round the number to the bottom
+print(abs(-3.14))
+#to know how many difference between the number and 0
+print(pow(pi, 2))
+#to square up the number
+print((math.sqrt(pi)))
+#to know the square root from the number
+print(max(x,y,z))
+#to know which is the highest number
+print(min(x,y,z))
+#to know which is the lowest number
