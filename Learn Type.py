b4a44bb48de76f9ca81db7802dbad05f3a59aec8
INewBieZ <99117553+INewBieZ@users.noreply.github.com>
2022-02-23 17:52:49 +0700

    Create Learn Type.py

---
+++ Learn Type.py
@@ -0,0 +1,26 @@
+#No. 1 string type
+#first_name = "Alfred"
+#last_name = "Alfred"
+#full_name = first_name +" "+ last_name
+#print("Hello " +full_name)
+#print(type(name)) = to check the type
+
+#No. 2 Interger type
+#age = 21
+#age = age + 1
+#age += 1
+#print("Your age is : " +str(age))
+#if you need display of different data type to need to wrin strcast to convert it.
+#print(type(age))
+
+#No. 3 Float type = can store a number that have decimal
+#height = 250.5
+#print("Your height is: "+str(height)+ " cm")
+#print(type(height))
+
+#No. 4 Boolean type = only can store true or false
+#human = True
+#print("Are you a human: " +str(human))
+#print(type(human))
+
+
