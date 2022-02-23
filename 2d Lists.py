e0ddee6d5f784ded0621ed22e5b5b934b9e35c9c
INewBieZ <99117553+INewBieZ@users.noreply.github.com>
2022-02-23 17:58:56 +0700

    Delete 2d Lists.py

--- 2d Lists.py
+++
@@ -1,9 +0,0 @@
-#2d lists = a list of lists
-
-dinners = ["pasta", "pizza", "chicken", "burger"]
-drinks = ["tea", "coffee", "water"]
-desserts = ["pudding", "ice cream"]
-
-foods = [dinners, drinks, desserts]
-
-print(foods[0][2])
