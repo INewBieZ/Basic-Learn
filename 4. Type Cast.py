9faeaf7dd8d01fe94581f47fbd6ff4e1c4ceed4a
INewBieZ <99117553+INewBieZ@users.noreply.github.com>
2022-02-23 17:59:31 +0700

    Create 4. Type Cast.py

---
+++ 4. Type Cast.py
@@ -0,0 +1,20 @@
+#type casting = convert the data type of a value to another data type
+
+#x = 1   #int
+#y = 2.0 #float
+#z = "3" #str cant perform any math on strings
+
+#y = int(y)
+#this is how to change it permanently
+#z = int(z)
+
+#print(x)
+#print(y)
+#print(int(y))
+#print(z*3) = 333 because its a string
+#print(z*3) = 9 because now its a int
+
+#when you need to change int or float to str
+#that is when we need to print it with a string
+#print("X is " +str(x)) = this how to do it
+#print("Y is " +y) = it will give an error
