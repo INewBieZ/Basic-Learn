47320c2b2be78ecfe642010c6c3cd59a957655cd
INewBieZ <99117553+INewBieZ@users.noreply.github.com>
2022-02-23 17:59:41 +0700

    Create 7. String Slicing.py

---
+++ 7. String Slicing.py
@@ -0,0 +1,40 @@
+#slicing = create a substring by extracting from other string
+#          indexing[] and slicing ()
+#          [start:stop:step]
+
+#name = "Alfred Alfred"
+#first_name = name[0:6]
+#last_name = name[7:13]
+#funky_name = name[0:13:2]
+#reversed_name = name[::-1]
+#remember the stop one is excluded
+#step is use to write every how many character include the first character
+
+#print(first_name)
+#print(last_name)
+#print(funky_name)
+#print(reversed_name)
+
+#name = "Bro Code"
+
+#reversed_name_1 = name[::-1]
+#reversed_name_2 = name[0:8:-2]
+
+#print(reversed_name_1)
+
+#print(reversed_name_2)
+#dont know why but this was not work
+
+website1 = "http://google.com"
+website2 = "http://wikipedia.com"
+
+slice = slice(7,-4)
+
+print(website1[slice])
+print(website2[slice])
+
+
+
+
+
+
