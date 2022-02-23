3b5fa002a5f639346afad689afd3c4a60662a7b2
INewBieZ <99117553+INewBieZ@users.noreply.github.com>
2022-02-23 17:55:09 +0700

    Create 2d Lists.py

---
+++ 2d Lists.py
@@ -0,0 +1,9 @@
+#2d lists = a list of lists
+
+dinners = ["pasta", "pizza", "chicken", "burger"]
+drinks = ["tea", "coffee", "water"]
+desserts = ["pudding", "ice cream"]
+
+foods = [dinners, drinks, desserts]
+
+print(foods[0][2])
